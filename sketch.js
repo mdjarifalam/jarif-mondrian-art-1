9106f54b44190b8efdaec52506a16975af34134b
mdjarifalam <115085161+mdjarifalam@users.noreply.github.com>
2024-05-17 05:29:06 +0600

    Sketch Upload 1

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let w = 500;
+let h = 500;
+
+function setup() {
+  createCanvas(w, h);
+  strokeWeight(5);
+  stroke(30);
+  noLoop();
+}
+
+function draw() {
+  mondr(0,0,w,h,5);
+}
+
+function mondr(x,y,wid,ht,iterations){
+  if (iterations > 0){
+    
+    if (random(1) < .5){
+      let r = random(0.20,0.80);
+      mondr(x,y,wid*r,ht, iterations-1);
+      mondr(x+wid*r,y,wid*(1-r),ht, iterations-1);
+    }
+    else {
+      let r = random(0.20,0.50);
+      mondr(x,y,wid,ht*r, iterations-1);
+      mondr(x,y+ht*r,wid, (1-r)*ht,  iterations-1);
+    }
+  }
+  else {
+    fill(255);
+    let colors = [[245,15,15], [13,127,190], [250,227,23],[0,0,0]];
+    if (random(1) <.6){
+      fill(random(colors));
+    }
+    rect (x,y,wid,ht);
+  }
+}
